cbc52010443b97a1186bd8a837327023838b08e8
Jonathan Maier <jonathan.maier@student.tugraz.at>
2025-05-11 20:11:42 +0200

    Fromatted output csv to align with requirements

--- frontend/src/components/Buttons/DownLoadButton.vue
+++ frontend/src/components/Buttons/DownLoadButton.vue
@@ -1,9 +1,10 @@
 <template>
-  <button @click="downloadCSV">Download CSV</button>
+  <BasicButton @click="downloadCSV">Download CSV</BasicButton>
 </template>
 
 <script setup>
 import { defineProps } from "vue";
+import BasicButton from "./BasicButton.vue";
 
 const props = defineProps({
   jsonData: {
@@ -14,17 +15,49 @@ const props = defineProps({
 
 function downloadCSV() {
   const data = props.jsonData;
-  const keys = Object.keys(data);
-  const values = keys.map((key) => JSON.stringify(data[key]));
 
-  const csvContent = keys.join(",") + "\n" + values.join(",");
+  const headers = [
+    "ID",
+    "Novelty (Score)",
+    "Novelty (Rationale)",
+    "Usefulness (Score)",
+    "Usefulness (Rationale)",
+    "Market Potential (Score)",
+    "Market Potential (Rationale)",
+    "Applicability (Score)",
+    "Applicability (Rationale)",
+    "Complexity (Score)",
+    "Complexity (Rationale)",
+    "Completeness (Score)",
+    "Completeness (Rationale)",
+    "General Feedback",
+  ];
+
+  const values = [
+    data.project_id, // renamed to "ID"" in CSV
+    data.novelty,
+    data.novelty_justification,
+    data.usefulness,
+    data.usefulness_justification,
+    data.market_potential,
+    data.market_potential_justification,
+    data.applicability,
+    data.applicability_justification,
+    data.complexity,
+    data.complexity_justification,
+    data.completeness,
+    data.completeness_justification,
+    data.feedback,
+  ].map((val) => JSON.stringify(val ?? ""));
+
+  const csvContent = headers.join(",") + "\n" + values.join(",");
 
   const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
   const url = URL.createObjectURL(blob);
 
   const link = document.createElement("a");
   link.setAttribute("href", url);
-  link.setAttribute("download", "data.csv");
+  link.setAttribute("download", "evaluation.csv");
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
